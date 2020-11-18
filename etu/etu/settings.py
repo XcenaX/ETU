17590d346c919ae698b2bc56b94f613fceff8ddd
XcenaX <vlad-057@mail.ru>
2020-11-18 11:22:35 +0600

    static files added

--- etu/etu/settings.py
+++ etu/etu/settings.py
@@ -139,7 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
