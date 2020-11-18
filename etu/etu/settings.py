04c8ec4a3fe3f43142eaf44f948266028df954c5
XcenaX <vlad-057@mail.ru>
2020-11-18 14:06:15 +0600

    static changed

--- etu/etu/settings.py
+++ etu/etu/settings.py
@@ -143,6 +143,6 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
